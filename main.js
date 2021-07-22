9e6a394323d43874e9a1780a48ea0ae1d259822f
FloriPro <floriprolohner@gmail.com>
2021-07-22 21:08:15 +0200

    Updated

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-window.addEventListener('load', function () {
-    console.log(window.scrollHeight);
-})
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
+
+/*function up() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}*/
