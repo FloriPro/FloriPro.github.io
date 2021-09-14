c9e8727f28087de5a9a77ad32cd966bb70b8a610
FloriPro <floriprolohner@gmail.com>
2021-09-14 16:26:24 +0200

    added hello message

--- cookie.js
+++ cookie.js
@@ -20,9 +20,11 @@ function getCookie(name) {
 }
 
 function AcceptCookie() {
-    document.cookie = "hasAccepted=True";
-    document.getElementsByClassName('CookieAccept')[0].style.display = "none";
+    document.getElementsByClassName('alert')[0].style.display='block';
+    document.getElementsByClassName('alert')[0].style.marginTop = "0px";
+    document.cookie = "hasAccepted=True; expires=Fri, 31 Dec 9999 23:59:59 GMT";
     document.getElementsByClassName('Site')[0].style.display = "block";
+    fadeOutEffect()
 }
 
 
@@ -32,8 +34,41 @@ window.addEventListener('load', function () {
         document.getElementsByClassName('CookieAccept')[0].style.display = "none";
     }
     else {
-        console.log("none");
         document.getElementsByClassName('Site')[0].style.display = "none";
+        document.getElementById("CookieAccept").style.opacity=1;
         document.getElementsByClassName('CookieAccept')[0].style.display = "block";
     }
 })
+
+
+//fadeOutWelcome Message
+function moveAwayEffect() {
+    var moveAwayTarget = document.getElementsByClassName("alert")[0];
+    var moveAwayTargetStyleMarginTopI=0
+    moveAwayTarget.style.marginTop = "0px";
+    var moveAwayEffect = setInterval(function () {
+        if (moveAwayTargetStyleMarginTopI > -(moveAwayTarget.offsetHeight+10)) {
+            moveAwayTargetStyleMarginTopI-=1
+            moveAwayTarget.style.marginTop = moveAwayTargetStyleMarginTopI+"px";
+        } else {
+            clearInterval(moveAwayEffect);
+            document.getElementsByClassName('alert')[0].style.display = "none";
+        }
+    }, 10);
+}
+
+//fade Out Cookie Message
+function fadeOutEffect() {
+    var fadeTarget = document.getElementById("CookieAccept");
+    var fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity > 0) {
+            fadeTarget.style.opacity -= 0.01;
+        } else {
+            clearInterval(fadeEffect);
+            document.getElementsByClassName('CookieAccept')[0].style.display = "none";
+        }
+    }, 1);
+}
